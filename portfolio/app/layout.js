40cf8f3405044aec2c489d16152dfd95f20ba66a
Talha Tanvir <talhatanveer202@gmail.com>
2025-06-13 22:35:13 +0500

    Add Project Files

---
+++ portfolio/app/layout.js
@@ -0,0 +1,29 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfitFont = Outfit({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+const ovoFont = Ovo({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Talha-Tanvir Portfolio",
+  description: "A personal portfolio website showcasing my projects, skills, and experience as a web developer.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfitFont.className} ${ovoFont.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
